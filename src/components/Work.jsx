1bc810c0dc9cc466b9991e3204c12ce24906799e
Ahmed <a.tamer87@yahoo.com>
2022-05-11 23:02:18 +0200

    Work Added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import workImg from "../assets/workImg1.png";
 import workImg2 from "../assets/workImg2.png";
+import workImg3 from "../assets/workImg3.png";
 import workImg4 from "../assets/workImg4.png";
-import workImg5 from "../assets/workImg5.png";
+import workImg5 from '../assets/workImg5.png';
 
 const Work = () => {
   return (
@@ -47,15 +48,15 @@ const Work = () => {
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Expenses Dashboard
+                React Shutter landing
               </span>
               <div className="pt-8 text-center">
-                <a href="https://ahmedxxta.github.io/React-Dashboard/">
+                <a href="https://ahmedxxta.github.io/react-shutter-landing-page/">
                   <button className="text-center text-lg rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold">
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/AhmedxxTa/React-Dashboard">
+                <a href="https://github.com/AhmedxxTa/react-shutter-landing-page">
                   <button className="text-center text-lg rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold">
                     Code
                   </button>
@@ -66,7 +67,7 @@ const Work = () => {
 
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${workImg5})` }}
+            style={{ backgroundImage: `url(${workImg4})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -90,7 +91,7 @@ const Work = () => {
 
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${workImg4})` }}
+            style={{ backgroundImage: `url(${workImg3})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -104,6 +105,30 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/AhmedxxTa/Vanilla-Music-Player">
+                  <button className="text-center text-lg rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${workImg5})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Expenses Dashboard
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://ahmedxxta.github.io/React-Dashboard/">
+                  <button className="text-center text-lg rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AhmedxxTa/React-Dashboard/">
                   <button className="text-center text-lg rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold">
                     Code
                   </button>
